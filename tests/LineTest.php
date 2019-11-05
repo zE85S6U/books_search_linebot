bfa4aeb0f13b795486df4f3c95d935aa5ae97194
zE85S6U <renraku.saki.saki@gmail.com>
2019-11-05 18:52:56 +0900

    テストのテスト

--- tests/LineTest.php
+++ tests/LineTest.php
@@ -6,10 +6,33 @@ use PHPUnit\Framework\TestCase;
 
 class LineTest extends TestCase
 {
+    protected $line;
 
-    public function testStickerType()
+    public function setUp(): void
     {
-        $line = new Line();
-        $this->assertNotEmpty($line->stickerType());
+        $this->line = new Line();
+    }
+
+
+    /**
+     * @dataProvider additionProvider
+     * @param $data
+     */
+    public function test_trimSearchData($data)
+    {
+        $expected = ['type' => 'text', 'text' => "見つかりませんでした..."];
+
+        $result = $this->line->trimSearchData($data);
+
+        $this->assertSame($result, $expected);
+    }
+
+    public function additionProvider()
+    {
+        return [
+            [0, 0, 0],
+            [0, 1, 1],
+            [1, 0, 1]
+        ];
     }
 }
