79b9f9bf123f20c7947b02974b84fff3827b8dd8
zE85S6U <renraku.saki.saki@gmail.com>
2020-03-08 22:51:01 +0900

    Messagesクラステストの追加

---
+++ tests/Utils/MessagesTest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Tests\Utils;
+
+use App\Utils\Messages;
+use PHPUnit\Framework\TestCase;
+
+class MessagesTest extends TestCase
+{
+
+
+
+    public function additionProvider()
+    {
+        return [
+
+        ];
+    }
+
+    public function testGetUserId()
+    {
+
+    }
+
+    public function test__construct()
+    {
+
+    }
+
+    public function testGetReplyToken()
+    {
+
+    }
+
+    public function testGetMessage()
+    {
+
+    }
+
+    public function testGetMessageType()
+    {
+
+    }
+}
